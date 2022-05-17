fae89a1cb8207a1a77c1f53296af43083abc4281
Jose <escobedo.jose48@outlook.com>
2022-05-17 13:32:38 -0700

    Adding Navbar and SideBar styled components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Phonify</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="reviews">Reviews</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="phones">Phones</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/Login">Login</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarElements";
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="reviews">Reviews</SidebarLink>
+          <SidebarLink to="phones">Phones</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/login">Login</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
