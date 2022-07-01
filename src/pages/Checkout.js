075358f00028f3978d6da9e228620a85cab313d4
+ <escobedo.jose48@outlook.com>
2022-06-30 22:33:00 -0700

    removing useffect from checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -77,15 +77,6 @@ const Checkout = ({ addNewFormData, cartPhones, setCartPhones, cartItems }) => {
     setNavigateToggle(!navigateToggle);
   };
 
-  useEffect(() => {
-    fetch("https://phonify-app.herokuapp.com/Cart", {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then(setCartPhones);
-  }, [cartPhones.length]);
-
   return (
     <>
       {navigateToggle ? (
